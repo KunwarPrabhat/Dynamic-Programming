33fd44a3f0c4e424228dab66dc71bfbca0826cb2
Abhishek Anand <acedmicabhishek@gmail.com>
2024-12-29 23:37:58 +0530

    onestuff

---
+++ climbingStairs/a.cpp
@@ -0,0 +1,5 @@
+// The Climbing Stairs problem is a classic example of dynamic programming, where the goal is to find how many distinct ways you can climb to the top of a staircase given that you can take either 1 step, 2 steps, or any other valid step size.
+// 
+// Problem Statement:
+// You are climbing a staircase with n steps. Each time you can either climb 1 step, 2 steps, or k steps (where k can be any valid step size). How many distinct ways can you reach the top?
+
