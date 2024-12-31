bae4b398d07766bd82365c6221994c50a9b1c276
Raj Kunwar Prabhat <rajkunwarprabhat@gmail.com>
2025-01-01 01:32:28 +0530

    Update sol.cpp

--- Search Insert Position/sol.cpp
+++ Search Insert Position/sol.cpp
@@ -12,3 +12,26 @@
 // Example 3:
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int result = binarySearch(nums, 0, nums.size()-1, target );
+        return result;
+    }
+    int binarySearch(vector<int>& nums, int low, int high, int x){
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == x)
+                return mid;
+
+            if(nums[mid] < x)
+                low = mid +1;
+            
+            else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+};
